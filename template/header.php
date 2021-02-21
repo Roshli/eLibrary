ca9ca87edccb2c01177f4d9aeeb4642d1d52add0
roshli <roshliperera@gmail.com>
2021-02-21 19:51:05 +0530

    First commit

---
+++ template/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $title; ?></title>
+
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Titans eLibrary</a>
+        </div>
+
+        <!--/.navbar-collapse -->
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <!-- link to publiser_list.php -->
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Category</a></li>
+              <!-- link to books.php -->
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Books</a></li>
+              <!-- link to contacts.php -->
+              <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li>
+              <!-- link to shopping cart -->
+              <li><a href="#"><span class="glyphicon glyphicon-education"></span>&nbsp; About US</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+    <?php
+      if(isset($title) && $title == "Index") {
+    ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+
+      <div class="col-md-3 text-center">
+          <img class="img-responsive" src="./bootstrap/img/logo.png">
+        </div>
+        <h1>Welcome to Titans eLibrary</h1>
+        <h2>Today a READER, Tomorrow a LEADER<h2>
+        <p>Powerded by Leo Club of Pannipitiya Metro Titans</p>
+      </div>
+    </div>
+    <?php } ?>
+
+    <div class="container" id="main">
---
+++ template/newFooter.php
@@ -0,0 +1,211 @@
+<html>
+<style>
+
+.site-footer
+{
+  font-size:15px;
+  line-height:24px;
+  color:#737373;
+}
+.site-footer hr
+{
+  border-top-color:#bbb;
+  opacity:0.5
+}
+.site-footer hr.small
+{
+  margin:20px 0
+}
+.site-footer h6
+{
+  color:#fff;
+  font-size:16px;
+  text-transform:uppercase;
+  margin-top:5px;
+  letter-spacing:2px
+}
+.site-footer a
+{
+  color:#737373;
+}
+.site-footer a:hover
+{
+  color:#3366cc;
+  text-decoration:none;
+}
+.footer-links
+{
+  padding-left:0;
+  list-style:none
+}
+.footer-links li
+{
+  display:block
+}
+.footer-links a
+{
+  color:#737373
+}
+.footer-links a:active,.footer-links a:focus,.footer-links a:hover
+{
+  color:#3366cc;
+  text-decoration:none;
+}
+.footer-links.inline li
+{
+  display:inline-block
+}
+.site-footer .social-icons
+{
+  text-align:right
+}
+.site-footer .social-icons a
+{
+  width:40px;
+  height:40px;
+  line-height:40px;
+  margin-left:6px;
+  margin-right:0;
+  border-radius:100%;
+  background-color:#33353d
+}
+.copyright-text
+{
+  margin:0
+}
+@media (max-width:991px)
+{
+  .site-footer [class^=col-]
+  {
+    margin-bottom:30px
+  }
+}
+@media (max-width:767px)
+{
+  .site-footer
+  {
+    padding-bottom:0
+  }
+  .site-footer .copyright-text,.site-footer .social-icons
+  {
+    text-align:center
+  }
+}
+.social-icons
+{
+  padding-left:0;
+  margin-bottom:0;
+  list-style:none
+}
+.social-icons li
+{
+  display:inline-block;
+  margin-bottom:4px
+}
+.social-icons li.title
+{
+  margin-right:15px;
+  text-transform:uppercase;
+  color:#96a2b2;
+  font-weight:700;
+  font-size:13px
+}
+.social-icons a{
+  background-color:#eceeef;
+  color:#818a91;
+  font-size:16px;
+  display:inline-block;
+  line-height:44px;
+  width:44px;
+  height:44px;
+  text-align:center;
+  margin-right:8px;
+  border-radius:100%;
+  -webkit-transition:all .2s linear;
+  -o-transition:all .2s linear;
+  transition:all .2s linear
+}
+.social-icons a:active,.social-icons a:focus,.social-icons a:hover
+{
+  color:#fff;
+  background-color:#29aafe
+}
+.social-icons.size-sm a
+{
+  line-height:34px;
+  height:34px;
+  width:34px;
+  font-size:14px
+}
+.social-icons a.facebook:hover
+{
+  background-color:#3b5998
+}
+.social-icons a.twitter:hover
+{
+  background-color:#00aced
+}
+.social-icons a.linkedin:hover
+{
+  background-color:#007bb6
+}
+.social-icons a.dribbble:hover
+{
+  background-color:#ea4c89
+}
+.social-icons a.youtube:hover
+{
+  background-color:#DC143C
+}
+@media (max-width:767px)
+{
+  .social-icons li.title
+  {
+    display:block;
+    margin-right:0;
+    font-weight:600
+  }
+}
+
+
+</style>
+
+
+
+
+<body>
+<br>
+  <br>
+  <br>
+<!-- Site footer -->
+<footer class="site-footer">
+      <div class="container">
+
+			<hr>
+		</div>
+		<div class="container">
+			<div class="row">
+			<div class="col-md-8 col-sm-6 col-xs-12">
+				<p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
+			<a href="https://www.facebook.com/Leo-Club-of-Pannipitiya-Metro-Titans-107070650791353">Leo Club of Panninpitya Metro Titans</a>.
+				</p>
+			</div>
+
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<ul class="social-icons">
+				<li><a class="facebook" href="https://www.facebook.com/Leo-Club-of-Pannipitiya-Metro-Titans-107070650791353"><i class="fa fa-facebook"></i></a></li>
+				<li><a class="dribbble" href="https://www.instagram.com/pannipitiyametro_titans/?hl=en"><i class="fa fa-instagram"></i></a></li>
+				<li><a class="linkedin" href="https://www.linkedin.com/company/leo-club-of-pannipitiya-metro-titans?trk=public_post_follow-view-profile"><i class="fa fa-linkedin"></i></a></li> 
+				<li><a class="youtube" href="https://www.youtube.com/channel/UCNXIacOi3_8EWYP-p2kfAeg"><i class="fa fa-youtube"></i></a></li>   
+				</ul>
+			</div>
+			</div>
+		</div>
+		</div>
+</footer>
+</body>
+
+
+</html>
+
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
